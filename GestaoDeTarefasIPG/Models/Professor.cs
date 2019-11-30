e0221bd3d3f3b6ecea8c686dfa3947f832d9e922
aureopaquete <aureopaquete007@outlook.com>
2019-11-30 17:48:43 +0000

    Classe professor criada no modelo

---
+++ GestaoDeTarefasIPG/Models/Professor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class Professor
+    {
+
+        public int ProfessorId { get; set; }
+
+
+        [Required]
+        [StringLength(250)]
+        public string Nome { get; set; }
+
+
+        [Required]
+        public int Contato{ get; set; }
+
+
+        [Required]
+        public int Email { get; set; }
+
+    }
+}
