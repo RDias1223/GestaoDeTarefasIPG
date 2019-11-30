75bddc718300aad8667ab540e0301c9bdbe2cb0d
aureopaquete <aureopaquete007@outlook.com>
2019-11-30 19:49:38 +0000

    criado um botão pra professores

--- GestaoDeTarefasIPG/Migrations/20191130180409_professor.Designer.cs
+++ GestaoDeTarefasIPG/Migrations/20191130180409_professor.Designer.cs
@@ -2,14 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GestaoDeTarefasIPG.Migrations
 {
     [DbContext(typeof(GestaoDeTarefasDbContext))]
-    partial class GestaoDeTarefasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191130180409_professor")]
+    partial class professor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
