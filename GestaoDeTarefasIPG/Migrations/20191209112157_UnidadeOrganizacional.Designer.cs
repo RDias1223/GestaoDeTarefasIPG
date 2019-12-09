394917b3a8ceb85f8957d7ae7d02514b303588e1
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2019-12-09 11:25:43 +0000

    Criação da Migração a Base de dados

--- GestaoDeTarefasIPG/Migrations/20191209112157_UnidadeOrganizacional.Designer.cs
+++ GestaoDeTarefasIPG/Migrations/20191209112157_UnidadeOrganizacional.Designer.cs
@@ -2,14 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GestaoDeTarefasIPG.Migrations
 {
     [DbContext(typeof(GestaoDeTarefasDbContext))]
-    partial class GestaoDeTarefasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191209112157_UnidadeOrganizacional")]
+    partial class UnidadeOrganizacional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GestaoDeTarefasIPG/Migrations/20191209112157_UnidadeOrganizacional.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoDeTarefasIPG.Migrations
+{
+    public partial class UnidadeOrganizacional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnidadeOrganizacional",
+                columns: table => new
+                {
+                    UnidadeOrganizacionalID = table.Column<string>(nullable: false),
+                    Nome = table.Column<string>(nullable: false),
+                    Contacto = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnidadeOrganizacional", x => x.UnidadeOrganizacionalID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnidadeOrganizacional");
+        }
+    }
+}
