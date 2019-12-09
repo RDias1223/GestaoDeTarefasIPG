5b6941c6e00990d51b3307f3f61b4065aca09dec
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2019-12-09 10:42:06 +0000

    Adding a UnidadeOrganazacional models

---
+++ GestaoDeTarefasIPG/Models/UnidadeOrganizacional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class UnidadeOrganizacional
+    {
+
+        public string UnidadeOrganizacionalID { get; set; }
+
+        
+        public string Name { get; set; }
+
+        public string Contacto { get; set; }
+
+
+        public string Email { get; set; }
+
+
+    }
+}
