b81a4e4ec14f68e0532ae1893982d18596a46617
RDias1223 <rdrony43@gmail.com>
2020-01-05 21:20:29 +0000

    criação do modelo tarefa

---
+++ GestaoDeTarefasIPG/Models/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; } 
+
+        [Required(ErrorMessage = "Por favor, introduza o nome")]
+        [StringLength(maximumLength: 50, MinimumLength = 3)]
+        public string Nome{ get; set; }
+        [Required(ErrorMessage = "Por favor, introduza o valor da prioridade")]
+        [RegularExpression(@"([0-3]\d{0})", ErrorMessage = "prioridade inválida")]
+        public int Prioridade { get; set; }
+        
+    }
+}
