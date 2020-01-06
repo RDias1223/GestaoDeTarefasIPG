59ccf762e04a308fd0ebfd8b61444411ee156bca
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2020-01-06 11:05:03 +0000

    Atualização do modelo

--- GestaoDeTarefasIPG/Models/Servico.cs
+++ GestaoDeTarefasIPG/Models/Servico.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,13 @@ namespace GestaoDeTarefasIPG.Models
     public class Servico
     {
         public int ServicoID { get; set; }
+
+        [Required(ErrorMessage = "Por favor, Introduza o Nome")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage ="Por favor, Introduza o seu número de telemóvel ou telefone")]
         public string Contacto { get; set; }
+
+       
     }
 }
