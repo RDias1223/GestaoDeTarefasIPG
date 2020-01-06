c3d4f3e2e28181ec48ef10e480e5afb8fb3d0fdc
RDias1223 <rdrony43@gmail.com>
2020-01-06 11:59:24 +0000

    cargo

---
+++ GestaoDeTarefasIPG/Models/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class Cargo
+    {
+        public int CargoId { get; set; }
+
+        [Required(ErrorMessage = "Por favor, introduza o nome")]
+        [StringLength(maximumLength: 50, MinimumLength = 3)]
+        public string Nome { get; set; }
+        public int CargoChefeId { get; set; } //anotação para chave estrangeira
+        public string Cargchefe { get; set; }
+
+    }
+}
