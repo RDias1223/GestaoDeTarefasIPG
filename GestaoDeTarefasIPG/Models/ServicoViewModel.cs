ff114616b783d115550960115eeb01031f72aa39
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2020-01-15 03:48:51 +0000

    Criação  do Modelo Serviço para a Paginação

---
+++ GestaoDeTarefasIPG/Models/ServicoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class ServicoViewModel
+    {
+        IEnumerable<UnidadeOrganizacional> UnidadeOrganizacional { get; set; }
+        public PaginaViewModels Pagination { get; set; }
+        public string CurrentName { get; set; }
+
+        public string Nome { get; set; }
+        public string Contacto { get; set; }
+    }
+}
