cab31787dd40609849226fc8e9f666624a452167
aureopaquete <aureopaquete007@outlook.com>
2020-01-22 20:37:49 +0000

    Criada a classe ProfessorViewModels

---
+++ GestaoDeTarefasIPG/Models/ProfessorViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class ProfessorViewModels
+    {
+
+
+        public IEnumerable<Funcionario> Funcionarios { get; set; }
+
+        public PaginaViewModels Paginacao { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
