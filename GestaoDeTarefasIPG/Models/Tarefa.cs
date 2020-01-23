2bb67b373ae7129f95fe936a60d63c28201efc0b
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2020-01-23 01:09:57 +0000

    Criação do modelo Tarefas

---
+++ GestaoDeTarefasIPG/Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class Tarefa
+    {
+        public int TarefaID { get; set; }
+
+        [Required(ErrorMessage = "Por favor, Introduza o Nome da Tarefa")]
+        public string Nome { get; set; }
+
+        [StringLength(1000)]
+        [Display(Name = "Descrição")]
+        public string Descrição { get; set; }
+
+        public int FuncionarioId { get; set; }
+        public Funcionario Funcionario { get; set; }
+    }
+}
