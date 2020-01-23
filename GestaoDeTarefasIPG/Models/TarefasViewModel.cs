3a5dd15523870d658c5aa0654877a0ec1f96a42c
BernardoSada <BernardoSada@DESKTOP-JCDFI8T>
2020-01-23 02:32:26 +0000

    Criação da Modelo TarefasViewModel

---
+++ GestaoDeTarefasIPG/Models/TarefasViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoDeTarefasIPG.Models
+{
+    public class TarefasViewModel
+    {
+        public IEnumerable<Tarefa> Tarefa { get; set; }
+        public PaginaViewModels Pagination { get; set; }
+        public string CurrentName { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
